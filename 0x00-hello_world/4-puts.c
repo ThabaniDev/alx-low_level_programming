0c2a10705ea85d1fb0cbbe226b5e2928bf75892a
Thabani Takwena <thabzthegreat@gmail.com>
2023-02-17 13:37:08 +0200

    Fixing some formating issues

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *   * main - Entry point
- *     *
- *       * Return: 0 on success
+ * main - Entry point
+ *
+ * Return: 0 on success
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
